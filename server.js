35ae49ba6584195c1d41469b80bacc3548dc6daa
DeanoBones <Deano007@protonmail.com>
2025-03-24 19:26:42 +0000

    colourupdate

--- server.js
+++ server.js
@@ -14,18 +14,16 @@ wss.on('connection', (ws) => {
     const id = Date.now();
     players[id] = { 
         id, 
-        position: { 
-            x: Math.random() * gridSize - halfGrid, // -50 to 50
-            y: 0, 
-            z: Math.random() * gridSize - halfGrid  // -50 to 50
-        }, 
+        position: { x: 0, y: 0, z: 0 }, 
         rotation: 0, 
-        color: 0x00ffff 
+        color: 0x00ffff // Default cyan
     };
     ws.send(JSON.stringify({ type: 'init', id, players }));
 
     wss.clients.forEach(client => {
-        if (client !== ws) client.send(JSON.stringify({ type: 'newPlayer', player: players[id] }));
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'newPlayer', player: players[id] }));
+        }
     });
 
     ws.on('message', (message) => {
@@ -37,16 +35,22 @@ wss.on('connection', (ws) => {
                 rotation: data.rotation, 
                 newTrail: data.newTrail, 
                 spawnTime: data.spawnTime, 
-                color: data.color 
+                color: data.color || players[id].color 
             };
             wss.clients.forEach(client => {
-                if (client !== ws) client.send(JSON.stringify({ type: 'update', player: players[id] }));
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({ type: 'update', player: players[id] }));
+                }
             });
         }
     });
 
     ws.on('close', () => {
         delete players[id];
-        wss.clients.forEach(client => client.send(JSON.stringify({ type: 'removePlayer', id })));
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ type: 'removePlayer', id }));
+            }
+        });
     });
 });
