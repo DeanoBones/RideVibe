d988e1c8ca5447c0ef0e240d8956852bd65a0bf5
DeanoBones <Deano007@protonmail.com>
2025-03-27 15:31:43 +0000

    revert

--- server.js
+++ server.js
@@ -4,60 +4,56 @@ const app = express();
 const server = app.listen(process.env.PORT || 3000);
 const wss = new WebSocket.Server({ server });
 
-app.use(express.static('public'));
-
-const players = {};
-
-wss.on('connection', (ws) => {
+app.use(express.static('public'));const players = {};wss.on('connection', (ws) => {
     const id = Date.now();
     players[id] = { id };
     ws.send(JSON.stringify({ type: 'init', id }));
 
-    ws.on('message', (message) => {
-        const data = JSON.parse(message);
-        if (data.type === 'start') {
+ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    if (data.type === 'start') {
+        players[id] = {
+            id: data.id,
+            position: data.position,
+            rotation: data.rotation,
+            color: data.color,
+            speed: data.speed || 6
+        };
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ type: 'start', ...players[id] }));
+            }
+        });
+    } else if (data.type === 'update') {
+        if (players[id]) {
             players[id] = {
-                id: data.id,
+                id: data.id || id,
                 position: data.position,
                 rotation: data.rotation,
-                color: data.color,
-                speed: data.speed || 6,
-                jumped: data.jumped || false // Add jumped to start
+                color: data.color || players[id].color,
+                speed: data.speed || players[id].speed,
+                newTrail: data.newTrail
             };
             wss.clients.forEach(client => {
                 if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'start', ...players[id] }));
+                    client.send(JSON.stringify({ type: 'update', ...players[id] }));
                 }
             });
-        } else if (data.type === 'update') {
-            if (players[id]) {
-                players[id] = {
-                    id: data.id || id,
-                    position: data.position,
-                    rotation: data.rotation,
-                    color: data.color || players[id].color,
-                    speed: data.speed || players[id].speed,
-                    newTrail: data.newTrail,
-                    jumped: data.jumped || players[id].jumped || false // Add jumped to update
-                };
-                wss.clients.forEach(client => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: 'update', ...players[id] }));
-                    }
-                });
-            }
-        } else if (data.type === 'getPlayers') {
-            const currentPlayers = Object.values(players).filter(p => p.id !== id);
-            ws.send(JSON.stringify({ type: 'currentPlayers', players: currentPlayers }));
+        }
+    } else if (data.type === 'getPlayers') {
+        const currentPlayers = Object.values(players).filter(p => p.id !== id);
+        ws.send(JSON.stringify({ type: 'currentPlayers', players: currentPlayers }));
+    }
+});
+
+ws.on('close', () => {
+    delete players[id];
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'removePlayer', id }));
         }
     });
-
-    ws.on('close', () => {
-        delete players[id];
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify({ type: 'removePlayer', id }));
-            }
-        });
-    });
 });
+
+});
+
